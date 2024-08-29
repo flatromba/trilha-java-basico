91afb51136c6957cd3ac657b2e391b6a54e58c12
Flavio Tromba <flatromba1994@gmail.com>
2024-08-29 18:54:05 -0300

    estrturas condicionais

---
+++ estrutura-condicional/src/switchCase/PlanoOperadora.java
@@ -0,0 +1,16 @@
+package switchCase;
+
+public class PlanoOperadora {
+    
+    public static void main(String[] args) {
+        String plano="M"; // B/M/T
+        switch (plano) {
+            case "T":
+            System.out.println("5gb YouTube");
+            case "M":
+            System.out.println("WhatsApp e Instagram grátis");
+            case "B":
+            System.out.println("100 minutos de ligação");
+        }
+    }
+}
